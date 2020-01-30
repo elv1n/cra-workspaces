f60fd460c7ded36350e72d163ce1889a3e663479
Elvin Dzhavadov <elvin.d@outlook.com>
2020-01-30 15:20:31 +0200

    add typescript example

---
+++ example-ts/packages/test1/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Test1 = () => <div>Test1 is working</div>;
---
+++ example-ts/packages/test2/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Test2 = () => <div>Test2 is working</div>;
